b074cb8a351e106b329b3886710489fade8dd200
mnuhell <m.villagordovera@gmail.com>
2020-11-22 23:38:11 +0100

    register user

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -3,6 +3,7 @@ import { fetchWithoutToken } from '../helpers/fetch';
 import { types } from '../types/types';
 import Swal from 'sweetalert2';
 import withRseactContent from 'sweetalert2-react-content';
+import { applyMiddleware } from 'redux';
 
 const errorLogin = withRseactContent(Swal);
 
@@ -37,9 +38,38 @@ export const startLogin = (email, password) => {
 	}	
 }
 
-const login = ( user ) => ({
+const login = (user) => ({
 
 	type: types.authLogin,
 	payload: user
 
-})
+});
+
+
+export const startRegister = ( user ) => {
+
+	return async (dispatch) => {
+
+		const resp = await fetchWithoutToken('auth/new', user, 'POST');
+		const body = await resp.json();
+
+		if (body.ok) {
+			localStorage.setItem('token', body.token);
+			localStorage.setItem('token-init-date', new Date().getTime());
+
+			dispatch(login({
+				uid: body.uid,
+				name: body.name
+			}))
+			
+		} else {
+			Swal.fire({
+				title: 'Error!',
+				text: `${body.msg}`,
+				icon: 'error',
+				confirmButtonText: 'Salir'
+			})
+		}
+		
+	}
+}	
--- src/components/auth/Register.js
+++ src/components/auth/Register.js
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { startRegister } from '../../actions/auth';
 import Logo from '../header/logo/Logo';
 import useForm from '../hooks/useForm';
 
@@ -6,33 +8,60 @@ import useForm from '../hooks/useForm';
 
 export const Register = () => {
 
+    const dispatch = useDispatch();
+    const [error, setError] = useState(false);
+    const [textError, setTextError] = useState()
+
     const [formValues, handleInputChange] = useForm({
         name: "Manuel",
         email: 'm.villa@gamil.com',
-        telefono: 65034567890,
+        telefono: '65034567890',
+        role: '9358u409587',
         password: 'dev12345678',
         repeatPassword: 'dev12345678'
     });
 
-    console.log(formValues);
-
     const { name, email, telefono, password, repeatPassword } = formValues;
-
-
 
     const handleSubmitregister = (e) => {
         e.preventDefault();
         
+        if( password !== repeatPassword) {
+            setError(true)
+            setTextError('las contraseñas no coinciden');
+            return;
+        } 
+
+        if (name.trim() === "" || email === "" || password === "" || telefono === "") {
+            setError(true)
+            setTextError('Por favor, los campos no pueden estar vacios');
+            return;
+        }
+
+        dispatch(startRegister({
+            name: name,
+            email: email,
+            telefono: telefono,
+            password: password
+        }))
+    
     }
 
     return (
         <div className="flex justify-center flex-wrap content-center h-screen login bg-blue-500">
-                <div className="login__content sm:w-1/2 md:w-1/5">
+                <div className="login__content sm:w-1/2 md:w-1/4">
                 <div className="logo flex mb-5 justify-center">
                     <Logo width="200" altTitle="Airfit App" />
                 </div>
-                <form onSubmit={ handleSubmitregister } className="bg-blue-600 shadow-md rounded px-8 pt-6 pb-8 mb-4">
-                <div className="mb-4">
+                <form onSubmit={handleSubmitregister} className="bg-blue-600 shadow-md rounded px-8 pt-6 pb-8 mb-4">
+                    {
+                        error ? <div className="rounded error_message bg-red-400 py-2 px-8 mb-4 mx-auto">
+                                    <p className="text-xs text-white"> { textError }</p>
+                                </div>
+                               : ""
+                    }
+                    
+                    <div className="mb-4">
                         <label className="block text-blue-50 text-sm font-bold mb-2" htmlFor="name">
                             Nombre
                         </label>

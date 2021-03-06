5280f3a806d0c6530e0405e79b0b7936a492783f
mnuhell <m.villagordovera@gmail.com>
2021-03-06 12:27:20 +0100

    HasRouter in app

--- src/CrossfitApp.js
+++ src/CrossfitApp.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { store } from './store/store';
 import { Provider } from 'react-redux';
-
+import { HashRouter } from "react-router-dom";
 import RoutesApp from './routes/RoutesApp';
 
 
@@ -9,7 +9,9 @@ const CrossfitApp = () => {
 
     return (
         <Provider store={ store }>
-            <RoutesApp />
+            <HashRouter>
+                <RoutesApp />
+            </HashRouter>
         </Provider>
 
     )
